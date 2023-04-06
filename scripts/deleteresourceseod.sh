d94775814db3f8b047c10ff88008cac6c3a6af8d
NooraKazi <121911133+NooraKazi@users.noreply.github.com>
2023-04-06 15:53:46 -0500

    Update deleteresourceseod.sh

--- scripts/deleteresourceseod.sh
+++ scripts/deleteresourceseod.sh
@@ -1,20 +1,30 @@
- # !/bin/bash
+# !/bin/bash
 
-        # Set subscription
-          az account set --subscription ${{ env.subscription }}
+# Specify target resource group and tag
+subscription='7dcf7d82-02f5-4995-80d9-c84f88197343'
+resource_group='webapps3-dev-rg'
+tag='Action' 
 
-        # Get list of resources with specific tag
-          resource_list= 'az resource list --tag ${{ env.tag }} --subscription ${{ env.subscription }}'
+# Set subscription
+az account set --subscription ${subscription}
 
-        # Get number of resources to be deleted
-          num_resources='echo $resource_list | jq length'
-         
-        # Delete resources
-          for((i=0; i<$num_resources; i++)); do
-            # Get $i th resource name and type
-            name='echo $resource_list | jq .[$i].name | tr -d '"''
-            type='echo $resource_list | jq .[$i].type | tr -d '"''
+# Get list of resources with specific tag
+resource_list=`az resource list --tag ${tag} --subscription ${subscription}`
 
-            # Delete $i th resource
-            az resource delete -g ${{ env.resource_group }} -n ${name} --resource-type ${type}
-          done
+# Get number of resources to be deleted
+num_resources=`echo $resource_list | jq length`
+echo 'Found' $num_resources 'resources'
+
+# Delete resources
+for((i=0; i<$num_resources; i++)); do
+    # Get $i th resource name and type
+    name=`echo $resource_list | jq .[$i].name | tr -d '"'`
+    type=`echo $resource_list | jq .[$i].type | tr -d '"'`
+    
+    echo 'Deleting following resource ...'
+    echo 'resource name': $name
+    echo 'resource type': $type
+    
+    # Delete $i th resource
+    az resource delete -g ${resource_group} -n ${name} --resource-type ${type}
+done
